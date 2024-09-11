5e1c68adf689667430c577ff2a7b5d39085c7d07
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-09-11 17:50:36 +0530

    initial files added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './assets/Header.jsx'
+import Meme from './assets/Meme.jsx'
+
+export default function App() {
+    return(    
+            <div>
+                <Header />
+                <Meme />
+            </div>
+    )    
+}
+
